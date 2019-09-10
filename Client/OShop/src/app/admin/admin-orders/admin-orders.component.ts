f5aeee26a340442944cda0636f7fea841e0760c4
Mehedy-Hasan <codewithmehedy@gmail.com>
2019-09-10 09:00:23 +0600

    Navbar added

---
+++ Client/OShop/src/app/admin/admin-orders/admin-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-orders',
+  templateUrl: './admin-orders.component.html',
+  styles: []
+})
+export class AdminOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Client/OShop/src/app/order-success/order-success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-success',
+  templateUrl: './order-success.component.html',
+  styles: []
+})
+export class OrderSuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
